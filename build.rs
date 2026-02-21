16f7917ba219373a85552d790747e69b632c1fcc
Maxim Babichev <maksim.babichev95@gmail.com>
2026-02-21 16:55:05 +0300

    add features for tests

--- build.rs
+++ build.rs
@@ -1,10 +1,26 @@
 use std::env;
 use std::path::PathBuf;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+#[cfg(feature = "proto-build")]
+fn compile_protos() -> Result<(), Box<dyn std::error::Error>> {
+    // Use vendored protoc binary (no system protoc required)
+    unsafe {
+        env::set_var("PROTOC", protoc_bin_vendored::protoc_bin_path().unwrap());
+    }
+
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
     tonic_build::configure()
         .file_descriptor_set_path(out_dir.join("helloworld_descriptor.bin"))
         .compile_protos(&["tests/server/helloworld.proto"], &["tests/server"])?;
     Ok(())
+}
+
+#[cfg(not(feature = "proto-build"))]
+fn compile_protos() -> Result<(), Box<dyn std::error::Error>> {
+    // Skip proto compilation when proto-build feature is not enabled
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    compile_protos()
 }
